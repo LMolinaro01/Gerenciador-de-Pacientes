988afa868629830cbe9ce671186ea0bcfd21a2e1
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-04-22 16:47:27 -0300

    Mudança nos dados do p1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Pacientes p1 = new Pacientes("Leonardo", "Acupuntura", "12", "Masculino", 19, "Fabio", "1 por semana");
+        Pacientes p1 = new Pacientes("Leonardo", "Acupuntura", "21988998899", "Masculino", 19, "Fabio", "1 por semana");
         System.out.println(p1.toString());
     }
 }
-
-//arrumar validação do telefone
