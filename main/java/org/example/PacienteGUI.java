6291da8fc536182411b00834be8424e26151ab84
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-05-20 15:11:28 -0300

    Criação do CRUD na Interface (Falta correção de Bugs)

--- main/java/org/example/PacienteGUI.java
+++ main/java/org/example/PacienteGUI.java
@@ -10,15 +10,14 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
-
+import java.awt.BorderLayout;
 import java.util.List;
 
 
 public class PacienteGUI extends JFrame {
-    private JTable PacientesTable;
-    private JButton adicionarButton, editarButton, excluirButton;
+    private JButton cadastrarButton, editarButton, excluirButton;
+    private JTable pacientesTable;
 
     public static void iniciarGUI(){
         SwingUtilities.invokeLater(()->{
@@ -32,35 +31,151 @@ public class PacienteGUI extends JFrame {
         setSize(700,500);
         setLocationRelativeTo(null);
 
-        PacientesTable = new JTable();
-        JScrollPane scrollPane = new JScrollPane(PacientesTable);
+        pacientesTable = new JTable();
+        JScrollPane scrollPane = new JScrollPane(pacientesTable);
 
-        adicionarButton = new JButton("Adicionar");
+        cadastrarButton = new JButton("Adicionar");
         editarButton = new JButton("Editar");
         excluirButton = new JButton("Excluir");
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.add(adicionarButton);
+        buttonPanel.add(cadastrarButton);
         buttonPanel.add(editarButton);
         buttonPanel.add(excluirButton);
 
-        //adicionarButton.addActionListener(e -> PacienteDAO.adicionarPaciente(Paciente));
-        //editarButton.addActionListener(e -> PacienteDAO.atualizarPaciente(Paciente Paciente));
-        //excluirButton.addActionListener(e -> PacienteDAO.excluirPaciente(Paciente Paciente));
+        cadastrarButton.addActionListener(e -> cadastrarPaciente());
+        editarButton.addActionListener(e -> editarPaciente());
+        excluirButton.addActionListener(e -> deletarPaciente());
 
         getContentPane().add(scrollPane, BorderLayout.CENTER);
         getContentPane().add(buttonPanel, BorderLayout.SOUTH);
 
-        atualizarTabelaContatos();
+        atualizarTabelaPacientes();
 
     }
-    private void atualizarTabelaContatos() {
+    private void atualizarTabelaPacientes() {
         List<Pacientes> Pacientes = PacienteDAO.listarPacientes();
-        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"ID", "Nome", "Email"}, 0);
+        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"ID", "Nome", "Idade", "Celular", "Gênero", "Tratamento", "Frequencia (Semanal)", "Faixa Etária"}, 0);
         for (Pacientes Paciente : Pacientes) {
-            tableModel.addRow(new Object[]{Paciente.getId(), Paciente.getNome()});
+            tableModel.addRow(new Object[]{Paciente.getId(), Paciente.getNome(), Paciente.getCelular(),Paciente.getGenero(),Paciente.getTratamento(),Paciente.getFrequencia(), Paciente.getFaixaEtaria()});
         }
-        PacientesTable.setModel(tableModel);
+        pacientesTable.setModel(tableModel);
+    }
+
+    private void cadastrarPaciente(){
+        JTextField nomeField = new JTextField();
+        JTextField idadeField = new JTextField();
+        JTextField celularField = new JTextField();
+        JTextField tratamentoField = new JTextField();
+        JTextField generoField = new JTextField();
+        JTextField frequenciaField = new JTextField();
+
+        JPanel panel = new JPanel(new GridLayout(3, 2));
+        panel.add(new JLabel("Nome:"));
+        panel.add(nomeField);
+
+        panel.add(new JLabel("Idade:"));
+        panel.add(idadeField);
+
+        panel.add(new JLabel("Celular:"));
+        panel.add(celularField);
+
+        panel.add(new JLabel("Tratamento:"));
+        panel.add(tratamentoField);
+
+        panel.add(new JLabel("Gênero:"));
+        panel.add(generoField);
+
+        panel.add(new JLabel("Frequência:"));
+        panel.add(frequenciaField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Adicionar Contato", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        if (result == JOptionPane.OK_OPTION) {
+            String nome = nomeField.getText();
+            String celular = celularField.getText();
+            String idadeStr  = idadeField.getText();
+            String tratamento = tratamentoField.getText();
+            String genero = generoField.getText();
+            String frequencia = frequenciaField.getText();
+
+            if (!nome.isEmpty() && !celular.isEmpty() && !idadeStr .isEmpty() && !tratamento.isEmpty() && !genero.isEmpty() && !frequencia.isEmpty() ) {
+                int idade = Integer.parseInt(idadeStr);
+                Pacientes novoPaciente = new Pacientes();
+                novoPaciente.setNome(nome);
+                novoPaciente.setCelular(celular);
+                novoPaciente.setIdade(idade);
+                novoPaciente.setTratamento(tratamento);
+                novoPaciente.setGenero(genero);
+                novoPaciente.setFrequencia(frequencia);
+
+                PacienteDAO.adicionarPaciente(novoPaciente);
+                atualizarTabelaPacientes();
+            } else {
+                JOptionPane.showMessageDialog(this, "Por favor, preencha todos os campos necessários.");
+            }
+        }
+
+    }
+
+    public void editarPaciente(){
+        int linhaSelecionada = pacientesTable.getSelectedRow();
+        if (linhaSelecionada != -1){ // quando nenhuma linha está selecionada, retorna -1
+            int id = (int) pacientesTable.getValueAt(linhaSelecionada, 0);
+            String nome = JOptionPane.showInputDialog("Digite o novo nome do contato:");
+            String celular = JOptionPane.showInputDialog("Digite o novo número do celular:");
+            String idadeStr = JOptionPane.showInputDialog("Digite a nova idade:");
+            String tratamento = JOptionPane.showInputDialog("Digite o novo nome do tratamento:");
+            String genero = JOptionPane.showInputDialog("Digite o genero:");
+            String frequencia = JOptionPane.showInputDialog("Digite a nova frequência semanal do paciente:");
+
+            if (!nome.isEmpty() && !celular.isEmpty() && !idadeStr .isEmpty() && !tratamento.isEmpty() && !genero.isEmpty() && !frequencia.isEmpty() ) {
+                int idade = Integer.parseInt(idadeStr);
+                Pacientes novoPaciente = new Pacientes();
+                novoPaciente.setNome(nome);
+                novoPaciente.setCelular(celular);
+                novoPaciente.setIdade(idade);
+                novoPaciente.setTratamento(tratamento);
+                novoPaciente.setGenero(genero);
+                novoPaciente.setFrequencia(frequencia);
+
+                PacienteDAO.adicionarPaciente(novoPaciente);
+                atualizarTabelaPacientes();
+            } else {
+                JOptionPane.showMessageDialog(this, "Por favor, Selecione um Paciente.");
+            }
+        }
+    }
+
+    public void deletarPaciente() {
+        int selectedRow = pacientesTable.getSelectedRow();
+        if (selectedRow != -1) {
+            int id = (int) pacientesTable.getValueAt(selectedRow, 0);
+
+            // cria uma mensagem de confirmação
+            int confirm = JOptionPane.showConfirmDialog(
+                    this,
+                    "Você realmente deseja excluir este paciente?",
+                    "ATENÇÃO!",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.WARNING_MESSAGE
+            );
+
+            // verifica se o usuário confirmou
+            if (confirm == JOptionPane.YES_OPTION) {
+                PacienteDAO.excluirPaciente(id);
+                atualizarTabelaPacientes();
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Selecione um paciente para excluir.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PacienteDAO.criarTabela();
+        SwingUtilities.invokeLater(() -> {
+            new PacienteGUI().setVisible(true);
+        });
     }
 
 }
