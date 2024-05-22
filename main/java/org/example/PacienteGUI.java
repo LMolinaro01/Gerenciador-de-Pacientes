aa7d7abeac933ec473bb91c8fa581682c4bdb596
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-05-22 17:31:58 -0300

    Cores nos botões + Melhoria no Tamanho. Agora a tabela não edita clicando 2x

--- main/java/org/example/PacienteGUI.java
+++ main/java/org/example/PacienteGUI.java
@@ -18,6 +18,9 @@ import javax.swing.BorderFactory;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Image;
+
 
 
 public class PacienteGUI extends JFrame {
@@ -33,13 +36,19 @@ public class PacienteGUI extends JFrame {
     public PacienteGUI() {
         setTitle("Gerenciador de Pacientes");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(740, 550);
+        setSize(650, 625);
         setLocationRelativeTo(null);
         setResizable(false);
 
+        Color buttonColor1 = new Color(0, 191, 124);
+        Color buttonColor2 = new Color(0, 159, 98);
+        Color buttonColor3 = new Color(0, 114, 67);
+        Color buttonColor4 = new Color(0, 81, 53);
+        Color buttonTextColor = Color.WHITE;
+        Color borderColor = new Color(0, 69, 44);
 
         JLabel tituloLabel = new JLabel("Gerenciador de Pacientes", JLabel.CENTER);
-        tituloLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        tituloLabel.setFont(new Font("Times New Roman", Font.BOLD, 26));
         tituloLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         ImageIcon icon = new ImageIcon("src/main/java/org/example/LOGO_Fisioterapia.jpg");
@@ -47,6 +56,8 @@ public class PacienteGUI extends JFrame {
         Image newimg = image.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH);
         icon = new ImageIcon(newimg);
         JLabel imagemLabel = new JLabel(icon, JLabel.CENTER);
+        imagemLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
 
         JPanel topPanel = new JPanel(new BorderLayout());
         topPanel.add(tituloLabel, BorderLayout.NORTH);
@@ -54,27 +65,43 @@ public class PacienteGUI extends JFrame {
 
         cadastrarButton = new JButton("Adicionar");
         cadastrarButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        //cadastrarButton.setSize(new Dimension(40, 10)); //largura/altura
+        cadastrarButton.setBackground(buttonColor1);
+        cadastrarButton.setForeground(buttonTextColor);
+        cadastrarButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        cadastrarButton.setFocusPainted(false); //remove o quadrado branco do adicionar
 
         editarButton = new JButton("Editar");
         editarButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-
-        //excluirButton = new JButton("Excluir");
-        //excluirButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        editarButton.setBackground(buttonColor2);
+        editarButton.setForeground(buttonTextColor);
+        editarButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        editarButton.setFocusPainted(false);
 
         exibirButton = new JButton("Exibir Pacientes");
         exibirButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        exibirButton.setBackground(buttonColor3);
+        exibirButton.setForeground(buttonTextColor);
+        exibirButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        exibirButton.setFocusPainted(false);
 
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 30, 30)); //(linha, coluna, padx, pady)
+        JButton fecharButton = new JButton("Fechar Programa"); //crei o botão de outra maneira
+        fecharButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        fecharButton.setBackground(buttonColor4);
+        fecharButton.setForeground(buttonTextColor);
+        fecharButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        fecharButton.setFocusPainted(false);
+
+
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 1, 30, 30)); //(linha, coluna, padx, pady)
         buttonPanel.add(cadastrarButton);
         buttonPanel.add(editarButton);
-        //buttonPanel.add(excluirButton);
         buttonPanel.add(exibirButton);
+        buttonPanel.add(fecharButton);
 
         cadastrarButton.addActionListener(e -> cadastrarPaciente());
         editarButton.addActionListener(e -> telaEditar());
-        //excluirButton.addActionListener(e -> deletarPaciente());
         exibirButton.addActionListener(e -> exibirPacientes());
+        fecharButton.addActionListener(e -> System.exit(0)); // fecha o programa
 
         getContentPane().add(topPanel, BorderLayout.NORTH);
         getContentPane().add(buttonPanel, BorderLayout.CENTER);
@@ -95,10 +122,8 @@ public class PacienteGUI extends JFrame {
                     paciente.getTratamento(),
                     paciente.getFaixaEtaria(),
                     paciente.getDia()
-
             });
         }
-
         pacientesTable.setModel(tableModel);
         pacientesTable.repaint();
 
@@ -166,30 +191,59 @@ public class PacienteGUI extends JFrame {
 
     private void telaEditar() {
         JFrame editarFrame = new JFrame("Editar Paciente");
-        editarFrame.setSize(600, 400);
+        editarFrame.setSize(600, 500);
         editarFrame.setLocationRelativeTo(null);
-        editarFrame.setResizable(false);;
+        editarFrame.setResizable(false);
 
-        pacientesTable = new JTable();
+        Color buttonColor1 = new Color(0, 191, 124);
+        Color buttonColor2 = new Color(0, 159, 98);
+        Color buttonColor3 = new Color(0, 114, 67);
+        Color buttonColor4 = new Color(0, 81, 53);
+        Color buttonTextColor = Color.WHITE;
+        Color borderColor = new Color(0, 69, 44);
+
+
+        pacientesTable = new JTable(){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // impede a edição de células clicando duas vezes
+            }
+        };
         JScrollPane scrollPane = new JScrollPane(pacientesTable);
-
-        editarFrame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        scrollPane.setPreferredSize(new Dimension(600, 300));
+        pacientesTable.getTableHeader().setReorderingAllowed(false); //não deixa arrastar as colunas
 
         JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 30, 20)); // Painel para conter os botões
 
         JButton editarPacienteButton = new JButton("Editar Paciente"); //criei o botão de outra forma (mais simples)
-        editarPacienteButton.addActionListener(e -> editarPaciente());
-        buttonPanel.add(editarPacienteButton);
+        editarPacienteButton.setBackground(buttonColor1);
+        editarPacienteButton.setForeground(buttonTextColor);
+        editarPacienteButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        editarPacienteButton.setFocusPainted(false);
 
         JButton deletarButton = new JButton("Deletar");
-        deletarButton.addActionListener(e -> deletarPaciente());
-        buttonPanel.add(deletarButton);
+        deletarButton.setBackground(buttonColor2);
+        deletarButton.setForeground(buttonTextColor);
+        deletarButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        deletarButton.setFocusPainted(false);
 
         JButton voltarButton = new JButton("Voltar");
-        voltarButton.addActionListener(e -> editarFrame.dispose());
+        voltarButton.setBackground(buttonColor3);
+        voltarButton.setForeground(buttonTextColor);
+        voltarButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        voltarButton.setFocusPainted(false);
+
+        buttonPanel.add(editarPacienteButton);
+        buttonPanel.add(deletarButton);
         buttonPanel.add(voltarButton);
 
-        editarFrame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        editarPacienteButton.addActionListener(e -> editarPaciente());
+        deletarButton.addActionListener(e -> deletarPaciente());
+        voltarButton.addActionListener(e -> editarFrame.dispose());
+
+        editarFrame.getContentPane().add(scrollPane, BorderLayout.NORTH);
+        editarFrame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
+
 
         atualizarTabelaPacientes();
         editarFrame.setVisible(true);
@@ -282,22 +336,37 @@ public class PacienteGUI extends JFrame {
 
     public void exibirPacientes(){
         JFrame exibirFrame = new JFrame("Editar Paciente");
-        exibirFrame.setSize(600, 400);
+        exibirFrame.setSize(600, 480);
         exibirFrame.setLocationRelativeTo(null);
         exibirFrame.setResizable(false);;
 
-        pacientesTable = new JTable();
+        Color buttonColor1 = new Color(0, 191, 124);
+        Color buttonTextColor = Color.WHITE;
+        Color borderColor = new Color(0, 69, 44);
+
+        pacientesTable = new JTable(){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // impede a edição de células clicando duas vezes
+            }
+        };
         JScrollPane scrollPane = new JScrollPane(pacientesTable);
+        scrollPane.setPreferredSize(new Dimension(600, 405));
+        pacientesTable.getTableHeader().setReorderingAllowed(false);
 
-        exibirFrame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        exibirFrame.getContentPane().add(scrollPane, BorderLayout.NORTH);
 
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 30, 20)); // Painel para conter os botões
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 1, 30, 20)); // Painel para conter os botões
 
         JButton voltarButton = new JButton("Voltar");
         voltarButton.addActionListener(e -> exibirFrame.dispose());
-        buttonPanel.add(voltarButton);
+        voltarButton.setBackground(buttonColor1);
+        voltarButton.setForeground(buttonTextColor);
+        voltarButton.setBorder(BorderFactory.createLineBorder(borderColor));
+        voltarButton.setFocusPainted(false);
 
-        exibirFrame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.add(voltarButton);
+        exibirFrame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
 
         atualizarTabelaPacientes();
         exibirFrame.setVisible(true);
@@ -332,6 +401,7 @@ public class PacienteGUI extends JFrame {
         SwingUtilities.invokeLater(() -> {
             new PacienteGUI().setVisible(true);
         });
+
     }
 
 }
